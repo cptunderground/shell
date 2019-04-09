82e313b2b86c1e5a499b84711d80c8ec51766d4b
cptunderground <jannik.jaberg@unibas.ch>
2019-04-09 21:16:36 +0200

    can send mails and read from JSON, still buggy

--- mailBOT/mail.sh
+++ mailBOT/mail.sh
@@ -1,34 +1,42 @@
 #!/bin/bash
 
-ARRAY=(IT 
-Matijewix 
-Gayan 
-"Dini Muetter" 
-Spasst 
-Mongo 
-Schwuchtel 
-Muschel)
+#parse
 
+INSULT=()
 insult="$(cat matijevic.json)"
-echo "${insult}" | jq '.'
-insult="$(cat matijevic.json)"
+echo "Found a total of: "$(echo "${insult}" | jq -r '. | length') "Elements"
+echo "${insult}" | jq -c '.[]'
+counter=0
 for row in $(echo "${insult}" | jq -r '.[] | @base64'); do
     _jq() {
      echo ${row} | base64 --decode | jq -r ${1}
     }
 
-   echo $(_jq '.name')
+   echo "reading: "$(_jq '.') "into ARRAY[$counter] as: $(_jq '.name') "
+   INSULT+=(\"$(_jq '.name')\")
+   counter=$(($counter+1))
 done
+echo "${INSULT[0]}"
+echo "${INSULT[1]}"
+echo "${INSULT[2]}"
+echo "${INSULT[3]}"
+echo "${INSULT[4]}"
+echo "${INSULT[5]}"
+echo "${INSULT[6]}"
 
 for ((i=1; i<=10; i++))
 do
-rand=$[$RANDOM % ${#ARRAY[@]}]
-echo ${ARRAY[$rand]}
+rand=$[$RANDOM % ${#INSULT[@]}]
 echo $i
-##{
-##    echo Subject: ${ARRAY[$rand]}
-##    echo "This is an automatic generated email with a random insult in the subject - please enjoy"
-##} | ssmtp dejan.matijevic@unibas.ch
-##sleep 30
+   if [ ${INSULT[$rand]} != "null" ]
+   then echo ${INSULT[$rand]}
+   #   {
+   #      echo Subject: ${INSULT[$rand]}
+   #      echo "This is an automatic generated email with a random insult in the subject - please enjoy"
+   #   } | ssmtp some@mail.com
+   #sleep 5
+   else echo "was null"
+   fi
 done
-#echo "hello World" | ssmtp -s "test" dejan.matijevic@unibas.ch
+
+echo "${INSULT[1]}"
